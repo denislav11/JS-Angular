fd1e7c6872b0fea50bcc242f8813dcfb83e24fa1
denncho <denislav_dimitrov11@abv.bg>
2018-01-24 00:15:21 -0800

    F

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         port: process.env.PORT || 3000,
-        dbPath: process.env.MONGODB_URI || 'mongodb://localhost:27017/online-store'
+        dbPath: process.env.MONGODB_URI
     },
     production: {}
 };
