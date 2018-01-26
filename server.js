75b7e27e68eba218c8e3eeca08ec9e48262aaf6a
denncho <denislav_dimitrov11@abv.bg>
2018-01-26 12:46:23 -0800

    Trying to connect heroku mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = require('./server/config/config')[env];
+console.log(env);
 console.log(config);
 require('./server/config/database')(config);
 const app = require('express')();
