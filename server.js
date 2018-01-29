f6b36a364455033d5e25dea5fa4cfc77df6691fb
denncho <denislav_dimitrov11@abv.bg>
2018-01-29 14:04:01 -0800

    Trying to add mongoose

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('./server/config/config')[env];
 console.log(env);
 console.log(config);
+console.log(process.env);
 require('./server/config/database')(config);
 const app = require('express')();
 require('./server/config/express')(app);
--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,10 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000,
+        port: 3000,
         dbPath: 'mongodb://localhost:27017/online-store'
     },
     production: {
         port: process.env.PORT || 8080,
-        dbPath: 
-		process.env.MONGODB_URI || 
-		process.env.MONGOLAB_URI ||
-		process.env.MONGOHQ_URL
+        dbPath: process.env.MONGODB_URI
     }
 };
